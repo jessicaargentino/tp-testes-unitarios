229efbb879d1aea4b40d6bd347f6060baccac576
Jéssica Argentino <jessicaargentino@hotmail.com>
2023-04-03 09:22:22 -0300

    feat: teste método que calcula média de inteiros

--- test/MediaTest.java
+++ test/MediaTest.java
@@ -16,20 +16,24 @@ public class MediaTest {
         Assert.assertEquals(Integer.valueOf(11), calculaMedia(Arrays.asList(-5, 14, 22, 0, -6, 45)));
     }
 
-    @Test(expected=ClassCastException.class)
-    public void deveRetornarClassCastException(){
-        List<? extends Number> list = Arrays.asList(5.2, 7.4, 8.5, 0, 9);
-        calculaMedia((List<Integer>) list);
+    @Test
+    public void deveRetornarMediaMenosDez(){
+        Assert.assertEquals(Integer.valueOf(-10), calculaMedia(Arrays.asList(-5, -3, -9, -25, -12)));
     }
 
-    @Test(expected=NumberFormatException.class)
-    public void deveRetornarClassCOastException(){
-        List<String> list = Arrays.asList("A", "5", "12", "C");
-        calculaMedia(Collections.singletonList(Integer.valueOf(list.toString())));
+    @Test
+    public void deveRetornarMediaVinteNove(){
+        Assert.assertEquals(Integer.valueOf(29), calculaMedia(Arrays.asList(2, 6, 22, 45, 70)));
     }
 
     @Test(expected= NullPointerException.class)
     public void deveRetornarNullPointerException(){
         Assert.assertNull(calculaMedia(Arrays.asList(null)));
+    }
+
+    @Test(expected=ClassCastException.class)
+    public void deveRetornarClassCastException(){
+        List<? extends Number> list = Arrays.asList(5.2, 7.4, 8.5, 0, 9);
+        calculaMedia((List<Integer>) list);
     }
 }
